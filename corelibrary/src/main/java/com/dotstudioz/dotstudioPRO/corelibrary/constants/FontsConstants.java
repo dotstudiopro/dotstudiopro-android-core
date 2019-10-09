cea3cf2c9d08f596e4167294986739f08ead9151
Mohsin Shaikh <mohsin@dotstudioz.com>
2019-10-09 17:14:44 +0530

    - Poppins otf added to FontsConstant  - Also enum added to FontsCoonstants

--- corelibrary/src/main/java/com/dotstudioz/dotstudioPRO/corelibrary/constants/FontsConstants.java
+++ corelibrary/src/main/java/com/dotstudioz/dotstudioPRO/corelibrary/constants/FontsConstants.java
@@ -78,6 +78,27 @@ public class FontsConstants implements Serializable {
     public static Typeface lilitaoneRegularFont;
     public static Typeface arimoRegularFont;
     public static Typeface arimoBoldFont;
+    public static Typeface poppinsBlack;
+    public static Typeface poppinsBlackItalic;
+    public static Typeface poppinsBold;
+    public static Typeface poppinsBoldItalic;
+    public static Typeface poppinsExtraBold;
+    public static Typeface poppinsExtraBoldItalic;
+    public static Typeface poppinsExtraLight;
+    public static Typeface poppinsExtraLightItalic;
+    public static Typeface poppinsItalic;
+    public static Typeface poppinsLight;
+    public static Typeface poppinsLightItalic;
+    public static Typeface poppinsMedium;
+    public static Typeface poppinsMediumItalic;
+    public static Typeface poppinsRegular;
+    public static Typeface poppinsSemiBold;
+    public static Typeface poppinsSemiBoldItalic;
+    public static Typeface poppinsThin;
+    public static Typeface poppinsThinItalic;
+
+
+
 
     public static void initializeInstance(Context ctx) {
 
@@ -149,5 +170,298 @@ public class FontsConstants implements Serializable {
         lilitaoneRegularFont = Typeface.createFromAsset(ctx.getResources().getAssets(), "lilitaone-regular.ttf");
         arimoBoldFont = Typeface.createFromAsset(ctx.getResources().getAssets(), "arimo-bold.ttf");
         arimoRegularFont = Typeface.createFromAsset(ctx.getResources().getAssets(), "arimo-regular.ttf");
+
+        poppinsBlack = Typeface.createFromAsset(ctx.getResources().getAssets(), "poppins-black.otf");
+        poppinsBlackItalic = Typeface.createFromAsset(ctx.getResources().getAssets(), "poppins-blackitalic.otf");
+        poppinsBold = Typeface.createFromAsset(ctx.getResources().getAssets(), "poppins-bold.otf");
+        poppinsBoldItalic = Typeface.createFromAsset(ctx.getResources().getAssets(), "poppins-bolditalic.otf");
+        poppinsExtraBold = Typeface.createFromAsset(ctx.getResources().getAssets(), "poppins-extrabold.otf");
+        poppinsExtraBoldItalic = Typeface.createFromAsset(ctx.getResources().getAssets(), "poppins-extrabolditalic.otf");
+        poppinsExtraLight = Typeface.createFromAsset(ctx.getResources().getAssets(), "poppins-extralight.otf");
+        poppinsExtraLightItalic = Typeface.createFromAsset(ctx.getResources().getAssets(), "poppins-extralightitalic.otf");
+        poppinsItalic = Typeface.createFromAsset(ctx.getResources().getAssets(), "poppins-italic.otf");
+        poppinsLight = Typeface.createFromAsset(ctx.getResources().getAssets(), "poppins-light.otf");
+        poppinsLightItalic = Typeface.createFromAsset(ctx.getResources().getAssets(), "poppins-lightitalic.otf");
+        poppinsMedium = Typeface.createFromAsset(ctx.getResources().getAssets(), "poppins-medium.otf");
+        poppinsMediumItalic = Typeface.createFromAsset(ctx.getResources().getAssets(), "poppins-mediumitalic.otf");
+        poppinsRegular = Typeface.createFromAsset(ctx.getResources().getAssets(), "poppins-regular.otf");
+        poppinsSemiBold = Typeface.createFromAsset(ctx.getResources().getAssets(), "poppins-semibold.otf");
+        poppinsSemiBoldItalic = Typeface.createFromAsset(ctx.getResources().getAssets(), "poppins-semibolditalic.otf");
+        poppinsThin = Typeface.createFromAsset(ctx.getResources().getAssets(), "poppins-thin.otf");
+        poppinsThinItalic = Typeface.createFromAsset(ctx.getResources().getAssets(), "poppins-thinitalic.otf");
     }
+
+
+    public enum FONTS_ENUM {
+        TFREGULAR,
+        TFBOLD,
+        TFSEMIBOLD,
+        TFMEDIUM,
+        ALWAYSFOREVERBOLD,
+        VERDANA,
+        VERDANAREGULAR,
+        GOTHAMBOOK,
+        GOTHAMMEDIUM,
+        GOTHAMBOLD,
+        SOURCESANSPROBOLD,
+        SOURCESANSPROREGULAR,
+        KLAVIKAREGULAR,
+        UNIVERSLTSTDULTRACN,
+        DIDACTGOTHICREGULAR,
+        LATOREGULAR,
+        LATOBOLD,
+        SFPROREGULAR,
+        SFPRODISPLAYREGULAR,
+        SFPROBOLD,
+        FUTURAMEDIUMBT,
+        FUTURAMEDIUMCONDENSEDBT,
+        FUTURACONDENSEDEXTRABOLD,
+        FUTURABOOKFONT,
+        FUTURALIGHTFONT,
+        AVENIRMEDIUMFONT,
+        AVENIRNEXTREGULARFONT,
+        MONTSERRATREGULARFONT,
+        MONTSERRATBOLDFONT,
+        MONTSERRATSEMIBOLDFONT,
+        MONTSERRATMEDIUMFONT,
+        OPENSANSREGULARFONT,
+        OPENSANSSEMIBOLDFONT,
+        OPENSANSBOLDFONT,
+        OPENSANSLIGHTFONT,
+        PROXIMANOVAEXTRACONDENSEDBLACKFONT,
+        PROXIMANOVAALTBOLDFONT,
+        PROXIMANOVAALTLIGHTFONT,
+        PROXIMANOVAALTTHINFONT,
+        PROXIMANOVABLACKFONT,
+        PROXIMANOVABOLDFONT,
+        PROXIMANOVAREGULARFONT,
+        PROXIMANOVATHINFONT,
+        ROBOTBLACKFONT,
+        ROBOTBOLDFONT,
+        ROBOTMEDIUMFONT,
+        ROBOTREGULARFONT,
+        ROBOTLIGHTFONT,
+        LATOREGULARFONT,
+        TFSITKAHEADINGREGULAR,
+        TFSITKAHEADINGBOLD,
+        TFSITKADISPLAYREGULAR,
+        TFSITKADISPLAYBOLD,
+        SITKA_HEADINGFONT,
+        SITKA_DISPLAYFONT,
+        KHANDBOLDFONT,
+        KHANDLIGHTFONT,
+        KHANDMEDIUMFONT,
+        KHANDREGULARFONT,
+        KHANDSEMIBOLDFONT,
+        COMFORTAABOLDFONT,
+        COMFORTAAREGULARFONT,
+        COMFORTAALIGHTFONT,
+        COMFORTAATHINFONT,
+        LILITAONEREGULARFONT,
+        ARIMOREGULARFONT,
+        ARIMOBOLDFONT,
+        POPPINSBLACK,
+        POPPINSBLACKITALIC,
+        POPPINSBOLD,
+        POPPINSBOLDITALIC,
+        POPPINSEXTRABOLD,
+        POPPINSEXTRABOLDITALIC,
+        POPPINSEXTRALIGHT,
+        POPPINSEXTRALIGHTITALIC,
+        POPPINSITALIC,
+        POPPINSLIGHT,
+        POPPINSLIGHTITALIC,
+        POPPINSMEDIUM,
+        POPPINSMEDIUMITALIC,
+        POPPINSREGULAR,
+        POPPINSSEMIBOLD,
+        POPPINSSEMIBOLDITALIC,
+        POPPINSTHIN,
+        POPPINSTHINITALIC
+    };
+    public Context context;
+    public Typeface getGetTypeFace(int fontsEnum) {
+        FontsConstants.initializeInstance(context);
+        if(fontsEnum == FONTS_ENUM.ALWAYSFOREVERBOLD.ordinal()) {
+            return FontsConstants.alwaysForeverBold;
+        } else if(fontsEnum == FONTS_ENUM.GOTHAMBOLD.ordinal()) {
+            return FontsConstants.gothamBold;
+        } else if(fontsEnum == FONTS_ENUM.PROXIMANOVABOLDFONT.ordinal()) {
+            return FontsConstants.proximaNovaBoldFont;
+        } else if(fontsEnum == FONTS_ENUM.ARIMOBOLDFONT.ordinal()) {
+            return FontsConstants.arimoBoldFont;
+        } else if(fontsEnum == FONTS_ENUM.ARIMOREGULARFONT.ordinal()) {
+            return FontsConstants.arimoRegularFont;
+        } else if(fontsEnum == FONTS_ENUM.AVENIRMEDIUMFONT.ordinal()) {
+            return FontsConstants.avenirMediumFont;
+        } else if(fontsEnum == FONTS_ENUM.AVENIRNEXTREGULARFONT.ordinal()) {
+            return FontsConstants.avenirNextRegularFont;
+        } else if(fontsEnum == FONTS_ENUM.COMFORTAABOLDFONT.ordinal()) {
+            return FontsConstants.comfortaaBoldFont;
+        } else if(fontsEnum == FONTS_ENUM.COMFORTAALIGHTFONT.ordinal()) {
+            return FontsConstants.comfortaaLightFont;
+        } else if(fontsEnum == FONTS_ENUM.COMFORTAAREGULARFONT.ordinal()) {
+            return FontsConstants.comfortaaRegularFont;
+        } else if(fontsEnum == FONTS_ENUM.COMFORTAATHINFONT.ordinal()) {
+            return FontsConstants.comfortaaThinFont;
+        } else if(fontsEnum == FONTS_ENUM.DIDACTGOTHICREGULAR.ordinal()) {
+            return FontsConstants.didactgothicRegular;
+        } else if(fontsEnum == FONTS_ENUM.FUTURABOOKFONT.ordinal()) {
+            return FontsConstants.futuraBookFont;
+        } else if(fontsEnum == FONTS_ENUM.FUTURACONDENSEDEXTRABOLD.ordinal()) {
+            return FontsConstants.futuraCondensedExtraBold;
+        } else if(fontsEnum == FONTS_ENUM.FUTURALIGHTFONT.ordinal()) {
+            return FontsConstants.futuraLightFont;
+        } else if(fontsEnum == FONTS_ENUM.FUTURAMEDIUMBT.ordinal()) {
+            return FontsConstants.futuraMediumBt;
+        } else if(fontsEnum == FONTS_ENUM.FUTURAMEDIUMCONDENSEDBT.ordinal()) {
+            return FontsConstants.futuraMediumCondensedBt;
+        } else if(fontsEnum == FONTS_ENUM.GOTHAMBOOK.ordinal()) {
+            return FontsConstants.gothamBook;
+        } else if(fontsEnum == FONTS_ENUM.GOTHAMMEDIUM.ordinal()) {
+            return FontsConstants.gothamMedium;
+        } else if(fontsEnum == FONTS_ENUM.KHANDBOLDFONT.ordinal()) {
+            return FontsConstants.khandBoldFont;
+        } else if(fontsEnum == FONTS_ENUM.KHANDLIGHTFONT.ordinal()) {
+            return FontsConstants.khandLightFont;
+        } else if(fontsEnum == FONTS_ENUM.KHANDMEDIUMFONT.ordinal()) {
+            return FontsConstants.khandMediumFont;
+        } else if(fontsEnum == FONTS_ENUM.KHANDREGULARFONT.ordinal()) {
+            return FontsConstants.khandRegularFont;
+        } else if(fontsEnum == FONTS_ENUM.KHANDSEMIBOLDFONT.ordinal()) {
+            return FontsConstants.khandSemiBoldFont;
+        } else if(fontsEnum == FONTS_ENUM.KLAVIKAREGULAR.ordinal()) {
+            return FontsConstants.klavikaRegular;
+        } else if(fontsEnum == FONTS_ENUM.LATOBOLD.ordinal()) {
+            return FontsConstants.latoBold;
+        } else if(fontsEnum == FONTS_ENUM.LATOREGULAR.ordinal()) {
+            return FontsConstants.latoRegular;
+        } else if(fontsEnum == FONTS_ENUM.LATOREGULARFONT.ordinal()) {
+            return FontsConstants.latoRegularFont;
+        } else if(fontsEnum == FONTS_ENUM.LILITAONEREGULARFONT.ordinal()) {
+            return FontsConstants.lilitaoneRegularFont;
+        } else if(fontsEnum == FONTS_ENUM.MONTSERRATBOLDFONT.ordinal()) {
+            return FontsConstants.montserratBoldFont;
+        } else if(fontsEnum == FONTS_ENUM.MONTSERRATMEDIUMFONT.ordinal()) {
+            return FontsConstants.montserratMediumFont;
+        } else if(fontsEnum == FONTS_ENUM.MONTSERRATREGULARFONT.ordinal()) {
+            return FontsConstants.montserratRegularFont;
+        } else if(fontsEnum == FONTS_ENUM.MONTSERRATSEMIBOLDFONT.ordinal()) {
+            return FontsConstants.montserratSemiBoldFont;
+        } else if(fontsEnum == FONTS_ENUM.OPENSANSBOLDFONT.ordinal()) {
+            return FontsConstants.opensansBoldFont;
+        } else if(fontsEnum == FONTS_ENUM.OPENSANSLIGHTFONT.ordinal()) {
+            return FontsConstants.opensansLightFont;
+        } else if(fontsEnum == FONTS_ENUM.OPENSANSREGULARFONT.ordinal()) {
+            return FontsConstants.opensansRegularFont;
+        } else if(fontsEnum == FONTS_ENUM.OPENSANSSEMIBOLDFONT.ordinal()) {
+            return FontsConstants.opensansSemiBoldFont;
+        } else if(fontsEnum == FONTS_ENUM.PROXIMANOVAALTBOLDFONT.ordinal()) {
+            return FontsConstants.proximaNovaAltBoldFont;
+        } else if(fontsEnum == FONTS_ENUM.PROXIMANOVAALTLIGHTFONT.ordinal()) {
+            return FontsConstants.proximaNovaAltLightFont;
+        } else if(fontsEnum == FONTS_ENUM.PROXIMANOVAALTTHINFONT.ordinal()) {
+            return FontsConstants.proximaNovaAltThinFont;
+        } else if(fontsEnum == FONTS_ENUM.PROXIMANOVABLACKFONT.ordinal()) {
+            return FontsConstants.proximaNovaBlackFont;
+        } else if(fontsEnum == FONTS_ENUM.PROXIMANOVAEXTRACONDENSEDBLACKFONT.ordinal()) {
+            return FontsConstants.proximaNovaExtraCondensedBlackFont;
+        } else if(fontsEnum == FONTS_ENUM.PROXIMANOVAREGULARFONT.ordinal()) {
+            return FontsConstants.proximaNovaRegularFont;
+        } else if(fontsEnum == FONTS_ENUM.PROXIMANOVATHINFONT.ordinal()) {
+            return FontsConstants.proximaNovaThinFont;
+        } else if(fontsEnum == FONTS_ENUM.ROBOTBLACKFONT.ordinal()) {
+            return FontsConstants.robotBlackFont;
+        } else if(fontsEnum == FONTS_ENUM.ROBOTBOLDFONT.ordinal()) {
+            return FontsConstants.robotBoldFont;
+        } else if(fontsEnum == FONTS_ENUM.ROBOTLIGHTFONT.ordinal()) {
+            return FontsConstants.robotLightFont;
+        } else if(fontsEnum == FONTS_ENUM.ROBOTMEDIUMFONT.ordinal()) {
+            return FontsConstants.robotMediumFont;
+        } else if(fontsEnum == FONTS_ENUM.ROBOTREGULARFONT.ordinal()) {
+            return FontsConstants.robotRegularFont;
+        } else if(fontsEnum == FONTS_ENUM.SFPROBOLD.ordinal()) {
+            return FontsConstants.sfProBold;
+        } else if(fontsEnum == FONTS_ENUM.SFPRODISPLAYREGULAR.ordinal()) {
+            return FontsConstants.sfProDisplayRegular;
+        } else if(fontsEnum == FONTS_ENUM.SFPROREGULAR.ordinal()) {
+            return FontsConstants.sfProRegular;
+        } else if(fontsEnum == FONTS_ENUM.SITKA_DISPLAYFONT.ordinal()) {
+            return FontsConstants.sitka_displayFont;
+        } else if(fontsEnum == FONTS_ENUM.SITKA_HEADINGFONT.ordinal()) {
+            return FontsConstants.sitka_headingFont;
+        } else if(fontsEnum == FONTS_ENUM.SOURCESANSPROBOLD.ordinal()) {
+            return FontsConstants.sourcesansproBold;
+        } else if(fontsEnum == FONTS_ENUM.SOURCESANSPROREGULAR.ordinal()) {
+            return FontsConstants.sourcesansproRegular;
+        } else if(fontsEnum == FONTS_ENUM.TFBOLD.ordinal()) {
+            return FontsConstants.tfBold;
+        } else if(fontsEnum == FONTS_ENUM.TFMEDIUM.ordinal()) {
+            return FontsConstants.tfMedium;
+        } else if(fontsEnum == FONTS_ENUM.TFREGULAR.ordinal()) {
+            return FontsConstants.tfRegular;
+        } else if(fontsEnum == FONTS_ENUM.TFSEMIBOLD.ordinal()) {
+            return FontsConstants.tfSemiBold;
+        } else if(fontsEnum == FONTS_ENUM.TFSITKADISPLAYBOLD.ordinal()) {
+            return FontsConstants.tfSitkaDisplayBold;
+        } else if(fontsEnum == FONTS_ENUM.TFSITKADISPLAYREGULAR.ordinal()) {
+            return FontsConstants.tfSitkaDisplayRegular;
+        } else if(fontsEnum == FONTS_ENUM.TFSITKAHEADINGBOLD.ordinal()) {
+            return FontsConstants.tfSitkaHeadingBold;
+        } else if(fontsEnum == FONTS_ENUM.TFSITKAHEADINGREGULAR.ordinal()) {
+            return FontsConstants.tfSitkaHeadingRegular;
+        } else if(fontsEnum == FONTS_ENUM.UNIVERSLTSTDULTRACN.ordinal()) {
+            return FontsConstants.universltstdUltracn;
+        } else if(fontsEnum == FONTS_ENUM.VERDANA.ordinal()) {
+            return FontsConstants.verdana;
+        } else if(fontsEnum == FONTS_ENUM.VERDANAREGULAR.ordinal()) {
+            return FontsConstants.verdanaRegular;
+        } else if(fontsEnum == FONTS_ENUM.POPPINSBLACK.ordinal()) {
+            return FontsConstants.poppinsBlack;
+        } else if(fontsEnum == FONTS_ENUM.POPPINSBLACKITALIC.ordinal()) {
+            return FontsConstants.poppinsBlackItalic;
+        } else if(fontsEnum == FONTS_ENUM.POPPINSBOLD.ordinal()) {
+            return FontsConstants.poppinsBold;
+        } else if(fontsEnum == FONTS_ENUM.POPPINSBOLDITALIC.ordinal()) {
+            return FontsConstants.poppinsBoldItalic;
+        } else if(fontsEnum == FONTS_ENUM.POPPINSEXTRABOLD.ordinal()) {
+            return FontsConstants.poppinsExtraBold;
+        } else if(fontsEnum == FONTS_ENUM.POPPINSEXTRABOLDITALIC.ordinal()) {
+            return FontsConstants.poppinsExtraBoldItalic;
+        } else if(fontsEnum == FONTS_ENUM.POPPINSEXTRALIGHT.ordinal()) {
+            return FontsConstants.poppinsExtraLight;
+        } else if(fontsEnum == FONTS_ENUM.POPPINSEXTRALIGHTITALIC.ordinal()) {
+            return FontsConstants.poppinsExtraLightItalic;
+        } else if(fontsEnum == FONTS_ENUM.POPPINSITALIC.ordinal()) {
+            return FontsConstants.poppinsLight;
+        } else if(fontsEnum == FONTS_ENUM.POPPINSLIGHT.ordinal()) {
+            return FontsConstants.poppinsLight;
+        } else if(fontsEnum == FONTS_ENUM.POPPINSLIGHTITALIC.ordinal()) {
+            return FontsConstants.poppinsLightItalic;
+        } else if(fontsEnum == FONTS_ENUM.POPPINSMEDIUM.ordinal()) {
+            return FontsConstants.poppinsMedium;
+        } else if(fontsEnum == FONTS_ENUM.POPPINSMEDIUMITALIC.ordinal()) {
+            return FontsConstants.poppinsMediumItalic;
+        } else if(fontsEnum == FONTS_ENUM.POPPINSREGULAR.ordinal()) {
+            return FontsConstants.poppinsRegular;
+        } else if(fontsEnum == FONTS_ENUM.POPPINSSEMIBOLD.ordinal()) {
+            return FontsConstants.poppinsSemiBold;
+        } else if(fontsEnum == FONTS_ENUM.POPPINSSEMIBOLDITALIC.ordinal()) {
+            return FontsConstants.poppinsSemiBoldItalic;
+        } else if(fontsEnum == FONTS_ENUM.POPPINSTHIN.ordinal()) {
+            return FontsConstants.poppinsThin;
+        } else if(fontsEnum == FONTS_ENUM.POPPINSTHINITALIC.ordinal()) {
+            return FontsConstants.poppinsThinItalic;
+        }
+
+        return FontsConstants.verdanaRegular;
+    };
+    
+    
+    
+    
+    
+    
+    
+    
 }
